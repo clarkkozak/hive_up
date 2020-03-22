59cc015ea180fb0a1d0964a320aca3ea3965730e
Clark Kozak <clarkkozak@protonmail.com>
2020-03-21 21:26:18 -0600

    Integrating dsteem
    Added
      - dsteem to handle content editing
        - couldn't figre out how to do it with curl
     - sucessfully editted the latest blog

---
+++ config.js
@@ -0,0 +1,13 @@
+const Mainnet = {
+  accounts: [],
+  url: 'https://api.steemit.com',
+  net: {
+      addressPrefix: 'STM',
+      chainId:
+          '0000000000000000000000000000000000000000000000000000000000000000',
+  },
+}
+
+module.exports = {
+  Mainnet
+}
--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 require('dotenv').config()
 const fetch = require('node-fetch')
+const { Client, PrivateKey } = require('dsteem')
+const { Mainnet  } = require('./config') //A Steem Testnet. Replace 'Testnet' with 'Mainnet' to connect to the main Steem blockchain.
+
+const diff_match_patch = require('diff-match-patch');
+const dmp = new diff_match_patch();
+
+let opts = { ...Mainnet.net };
+
+const privateKey = PrivateKey.fromString(process.env.POSTING_KEY)
+const client = new Client(Mainnet.url, opts);
+
+
+function createPatch(text, out) {
+  if (!text && text === '') return undefined;
+  const patch_make = dmp.patch_make(text, out);
+  const patch = dmp.patch_toText(patch_make);
+  return patch;
+}
+
 
 const username = process.env.STEEM_USER
+const edited_body = 'This content was edited by an the Steem API'
 
 const fetchSinglePost = `{
   "jsonrpc": "2.0",
@@ -40,14 +60,48 @@ const results = fetch('https://api.steemit.com', {
 })
   .then(res => res.json())
   .then(json => {
-    console.log(json)
     return json.result
   })
   .then(blogs => {
     const myBlogs = blogs.filter(blog => {
       return username === blog.comment.author
     })
-    console.log(myBlogs)
+
+    
+    const myLastestBlog = myBlogs[0]
+    const { comment } = myLastestBlog
+    console.log(myLastestBlog)
+    const { title, json_metadata, permlink } = comment
+    console.log(permlink)
+    
+    const o_body = comment.body
+    const tagList = JSON.parse(json_metadata)
+    
+    //computes a list of patches to turn o_body to edited_body
+      const patch = createPatch(o_body, edited_body);
+          //check if patch size is smaller than edited content itself
+    if (patch && patch.length < new Buffer(edited_body, 'utf-8').length) {
+      body = patch;
+  } else {
+      body = edited_body;
+  }
+
+  client.broadcast
+    .comment({
+      author: username,
+      body,
+      json_metadata,
+      parent_author: '',
+      parent_permlink: tagList.tags[0],
+      permlink,
+      title
+    },
+    privateKey
+    )
+    .then((result) => {
+      console.log(result)
+      console.log(`http://condenser.steem.vc/${taglist.tag[0]}/@${account}/${permlink}`)
+    })
   })
   .catch(err => console.log(err))
 
